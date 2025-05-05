5d0331fc3be5ca26d8b62fbd5246854c1e73baa7
calculator <kirill.irshenko@gmail.com>
2025-05-05 18:25:58 +0700

    New import

--- app/src/main/java/ru/hitsmobileapp/MainActivity.kt
+++ app/src/main/java/ru/hitsmobileapp/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.hitsmobileapp.ui.theme.HITsMobileAppTheme
